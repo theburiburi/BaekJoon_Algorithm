65dbd3c0be89b077d8d64131107499a327914764
theburiburi <kuda1390@naver.com>
2023-07-14 16:37:23 +0900

    1158

---
+++ 1158.py
@@ -0,0 +1,24 @@
+import sys
+
+num, gap = map(int, (sys.stdin.readline().split()))
+list = []
+
+start = gap-1
+step = start
+
+for i in range (num):
+    list.append(i+1)
+
+print("<", end="")
+
+for j in range(num):
+    print(list.pop(start), end ="")
+    start = start+step
+    if len(list) >0:
+        print(", ", end ="")
+    else :
+        break
+    if start > len(list)-1:
+        start = start % (len(list))
+    
+print(">", end ="")
