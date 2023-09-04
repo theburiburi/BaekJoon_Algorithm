899c09805c8da5c4e04502e37b3b692324602ea0
theburiburi <kuda1390@naver.com>
2023-09-04 20:06:53 +0900

    2579

---
+++ 2579.py
@@ -0,0 +1,17 @@
+import sys
+
+N = int(input())
+
+stair = [0] * 301
+dp = [0] * 301
+for i in range(N):
+    stair[i] = int(sys.stdin.readline())
+
+dp[0] = stair[0]
+dp[1] = stair[0] + stair[1]
+dp[2] = max(stair[0]+stair[2], stair[1]+stair[2])
+
+for i in range(3, N):
+    dp[i] = max(dp[i-3] + stair[i-1] + stair[i], dp[i-2] + stair[i])
+    
+print(dp[N-1])
